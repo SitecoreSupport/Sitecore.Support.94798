2364770bbffcdd7c03632407fb17156f30c8bb40
Dmitry Kolinchuk <dmko@sitecore.net>
2017-02-24 15:52:07 +0200

    Improve content tree search to open an item in a content language

---
+++ src/Sitecore.Support.94798/IDResolver.cs
@@ -0,0 +1,35 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Search;
+using Sitecore.Pipelines.Search;
+
+namespace Sitecore.Support.Pipelines.Search
+{
+    /// <summary>
+    /// Resolves id searches.
+    /// </summary>
+    public class IDResolver
+    {
+        /// <summary>
+        /// Runs the processor.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public void Process(SearchArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (!ID.IsID(args.TextQuery) || string.IsNullOrEmpty(args.TextQuery))
+            {
+                return;
+            }
+            Item item = args.Database.GetItem(args.TextQuery, args.ContentLanguage);
+            if (item != null)
+            {
+                SearchResult result = SearchResult.FromItem(item);
+                args.Result.AddResultToCategory(result, Translate.Text("Direct Hit"));
+            }
+            args.AbortPipeline();
+        }
+    }
+}
---
+++ src/Sitecore.Support.94798/PathResolver.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Search;
+using Sitecore.Pipelines.Search;
+
+namespace Sitecore.Support.Pipelines.Search
+{
+    /// <summary>
+    /// Resolves path searches
+    /// </summary>
+    public class PathResolver
+    {
+        /// <summary>
+        /// Runs the processor.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public void Process(SearchArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (!args.TextQuery.StartsWith("/", System.StringComparison.InvariantCulture) || string.IsNullOrEmpty(args.TextQuery))
+            {
+                return;
+            }
+            Item item = args.Database.GetItem(args.TextQuery, args.ContentLanguage);
+            if (item == null && !args.TextQuery.StartsWith("/sitecore", System.StringComparison.InvariantCulture))
+            {
+                item = args.Database.GetItem("/sitecore" + args.TextQuery, args.ContentLanguage);
+            }
+            if (item == null && !args.TextQuery.StartsWith("/sitecore/content", System.StringComparison.InvariantCulture))
+            {
+                item = args.Database.GetItem("/sitecore/content/" + args.TextQuery, args.ContentLanguage);
+            }
+            if (item == null)
+            {
+                return;
+            }
+            SearchResult result = SearchResult.FromItem(item);
+            args.Result.AddResultToCategory(result, Translate.Text("Direct Hit"));
+            if (!args.TextQuery.EndsWith("/", System.StringComparison.InvariantCulture))
+            {
+                return;
+            }
+            foreach (Item item2 in item.Children)
+            {
+                result = SearchResult.FromItem(item2);
+                args.Result.AddResultToCategory(result, Translate.Text("Children"));
+            }
+        }
+    }
+}
